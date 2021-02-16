c217c80ea3600df8ded661c9c73d6d9499eabb79
nikolichnik <nikolichnik.exe@gmail.com>
2021-02-16 16:13:26 +0100

    yesno dialog to verify arguments again again

--- arch/chroot.sh
+++ arch/chroot.sh
@@ -24,4 +24,4 @@ archetype() {
     dialog --yesno "\\nRunning Archetype with arguments -r $1 -b $2 -a $3" 10 50 && curl -O https://raw.githubusercontent.com/nikolichnik/archetype/master/archetype.sh && bash archetype.sh -r "$1" -b "$2" -a "$3" 
 }
 
-dialog --title "Archetype setup" --yesno "\\nArchetype install script will automatically install a full Arch Linux based desktop environment.\n\nIf you'd like to install this, select yes, otherwise select no."  10 60 && archetype
+dialog --title "Archetype setup" --yesno "\\nArchetype install script will automatically install a full Arch Linux based desktop environment with arguments -r $1 -b $2 -a $3.\n\nIf you'd like to install this, select yes, otherwise select no." 15 60 && archetype
