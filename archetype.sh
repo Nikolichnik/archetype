a44b922df6899b11a4c1e65afd00379804c710aa
nikolichnik <nikolichnik.exe@gmail.com>
2021-02-16 16:58:56 +0100

    Fixing scripts...

--- archetype.sh
+++ archetype.sh
@@ -6,7 +6,7 @@
 ### OPTIONS AND VARIABLES ###
 
 while getopts ":r:b:a:h" o; do case "${o}" in
-    r) archetyperepo=${OPTARG} && git ls-remote "$archetyperepo" || exit 1 ;;
+    r) archetyperepo=${OPTARG} && dialog --msgbox "$archetyperepo" 10 50 && git ls-remote "$archetyperepo" || exit 1 ;;
     b) repobranch=${OPTARG} ;;
     a) aurhelper=${OPTARG} ;;
     h) printf "Optional arguments for custom use:\\n  -r: Archetype repository\\n  -b: Archetype repository branch\\n  -a: AUR helper (must have pacman-like syntax)\\n  -h: Show this message\\n" && exit 1 ;;
